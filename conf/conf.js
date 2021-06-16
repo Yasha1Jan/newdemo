0a76decfb83c36200c5074c9c372e88961e6d30a
Yasha1Jan <55711264+Yasha1Jan@users.noreply.github.com>
2021-06-16 13:55:34 -0400

    Update conf.js

--- conf/conf.js
+++ conf/conf.js
@@ -1,24 +1,31 @@
 exports.config = {
-    //directConnect: true,
+   // directConnect: true,
    
      framework: 'jasmine2',
     seleniumServerStartTimeout: 90000,
-   // seleniumAddress: 'http://localhost:4444/wd/hub',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
      
    
      //capabilities: {
     ///   'browserName': 'chrome'
     // },
-     multiCapabilities: [{
-    browserName: 'chrome',  
-         maxInstances: 4,
-         
-          }],
+    multiCapabilities: [{'browserName': 'chrome'},  
+],                   
     specs: [
        '../specs/spec.js'
       
      ],
+     "highlightDelay": 1000,
+     "localSeleniumStandaloneOpts": {
+       "jvmArgs": [
+         "-Dwebdriver.ie.driver=node_modules/protractor/node_modules/webdriver-manager/selenium/IEDriverServer3.141.59.exe"
+       ]
+     },
+     
      onPrepare: async () => {
+      
+        browser.driver.ignoreSynchronization = true;
+        browser.waitForAngularEnabled(false);
      let AllureReporter = require('jasmine-allure-reporter');
     jasmine.getEnv().addReporter(new AllureReporter({
       resultsDir: 'reports/allure-results'
