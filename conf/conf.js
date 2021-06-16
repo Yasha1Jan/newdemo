89448b4176f128f4bde42dea310011462d5fc401
Yasha1Jan <sharmayasha1jan@gmail.com>
2021-06-16 08:48:28 -0400

    update

--- conf/conf.js
+++ conf/conf.js
@@ -1,13 +1,16 @@
 exports.config = {
-   //directConnect: true,
+   directConnect: false,
    
      framework: 'jasmine2',
   //  seleniumServerStartTimeout: 90000,
   //  seleniumAddress: 'http://localhost:4444/wd/hub',
-   seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  // seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
      
    
-   //DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+   multiCapabilities: [{
+    browserName: 'chrome',
+    maxInstances: 1
+   }],
    
                   
     specs: [
