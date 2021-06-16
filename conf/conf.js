af4d1d813742e0fdc76284dc531af53868483b9c
Yasha1Jan <sharmayasha1jan@gmail.com>
2021-06-16 12:52:38 -0400

    update

---
+++ conf/conf.js
@@ -0,0 +1,23 @@
+exports.config = {
+    directConnect: true,
+   
+     framework: 'jasmine2',
+    seleniumServerStartTimeout: 90000,
+     //seleniumAddress: 'http://localhost:4444/wd/hub',
+     
+   
+     //capabilities: {
+    ///   'browserName': 'chrome'
+    // },
+    capabilities: {'browserName': 'chrome'},                     
+    specs: [
+       '../specs/spec.js'
+      
+     ],
+     onPrepare: async () => {
+     let AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'reports/allure-results'
+    }));
+}
+   };
