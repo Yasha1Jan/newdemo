4fc4774c07989dbdf3564da738ac2c65241d33fd
Yasha1Jan <55711264+Yasha1Jan@users.noreply.github.com>
2021-06-16 16:43:09 -0400

    Update conf.js

--- conf/conf.js
+++ conf/conf.js
@@ -7,9 +7,7 @@ exports.config = {
    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
      
    
-   capabilities: {
-   'browserName': 'chrome'
-    },
+   DesiredCapabilities capabilities = DesiredCapabilities.chrome();
    
                   
     specs: [
