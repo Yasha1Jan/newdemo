3e440685236a8e9b6ff0f09b09e85a1419a8945d
Yasha1Jan <55711264+Yasha1Jan@users.noreply.github.com>
2021-06-23 16:44:21 -0400

    Update conf.js

--- conf/conf.js
+++ conf/conf.js
@@ -3,7 +3,7 @@ exports.config = {
    
      framework: 'jasmine2',
   //  seleniumServerStartTimeout: 90000,
- // seleniumAddress: 'http://localhost:4444/wd/hub',
+seleniumAddress: 'http://localhost:4444/wd/hub',
   // seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
  
    
