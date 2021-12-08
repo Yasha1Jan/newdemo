e4a5b77ed4e04addb5a9ae3cadb32993e8a6ada6
Yasha1Jan <55711264+Yasha1Jan@users.noreply.github.com>
2021-12-08 12:27:39 -0500

    Create page1.po.js

--- pages/page1.po.js
+++ pages/page1.po.js
@@ -10,7 +10,9 @@ const Homepage = (function () {
 
         getURL: function () {
             browser.sleep(10000);
-            browser.get('http://juliemr.github.io/protractor-demo/', '200000');
+          //  browser.get('http://juliemr.github.io/protractor-demo/');
+            browser.get('https://www.google.com/');
+            browser.sleep(10000);
         },
 
         goButton: async function () {
